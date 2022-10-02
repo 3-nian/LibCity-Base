346a8d83e06e9f4fcf686d8044ecd07ad008d9a8
poom-lee <zhenpengli@buaa.edu.cn>
2022-10-02 10:49:35 +0800

    version 1.2

--- libcity/model/traffic_od_prediction/MOMO.py
+++ libcity/model/traffic_od_prediction/MOMO.py
@@ -78,8 +78,8 @@ class CrissCrossAttention3D(nn.Module):
 
     def __init__(self, in_dim, verbose=False):
         super(CrissCrossAttention3D, self).__init__()
-        self.query_conv = nn.Conv3d(in_channels=in_dim, out_channels=in_dim // 8, kernel_size=1)
-        self.key_conv = nn.Conv3d(in_channels=in_dim, out_channels=in_dim // 8, kernel_size=1)
+        self.query_conv = nn.Conv3d(in_channels=in_dim, out_channels=in_dim // 4, kernel_size=1)
+        self.key_conv = nn.Conv3d(in_channels=in_dim, out_channels=in_dim // 4, kernel_size=1)
         self.value_conv = nn.Conv3d(in_channels=in_dim, out_channels=in_dim, kernel_size=1)
         self.softmax = nn.Softmax(dim=4)
         self.gamma = nn.Parameter(torch.zeros(1))
@@ -225,7 +225,7 @@ class MOMO(AbstractTrafficStateModel):
                                       nn.ReLU(inplace=False)
                                       )
         self.ST_Blocks = ST3DCCBlock(32, 32)
-        self.embed = nn.Sequential(nn.Conv2d(in_channels=160, out_channels=128,kernel_size=1),
+        self.embed = nn.Sequential(nn.Conv2d(in_channels=self.input_window*16, out_channels=128,kernel_size=1),
                                    nn.ReLU(inplace=False),
                                    nn.Conv2d(in_channels=128,out_channels=64,kernel_size=1),
                                    nn.ReLU(inplace=False))
--- libcity/model/traffic_od_prediction/__init__.py
+++ libcity/model/traffic_od_prediction/__init__.py
@@ -2,9 +2,11 @@ from libcity.model.traffic_od_prediction.CSTN import CSTN
 from libcity.model.traffic_od_prediction.GEML import GEML
 from libcity.model.traffic_od_prediction.CONVLSTM import CONVLSTM
 from libcity.model.traffic_od_prediction.ST3DNet import ST3DNet
+from libcity.model.traffic_od_prediction.MOMO import MOMO
 __all__ = [
     "GEML",
     "CSTN",
     "CONVLSTM",
-    "ST3DNet"
+    "ST3DNet",
+    "MOMO"
 ]
